49ffdc4b0a2bc204dda3170240d6c2d1598bff54
Xavier Perseguers <xavier@causal.ch>
2014-02-18 09:50:38 +0100

    [TASK] Do not autoload tt_news configuration

--- ext_tables.php
+++ ext_tables.php
@@ -1,14 +1,16 @@
 <?php
-if (!defined ('TYPO3_MODE'))
-	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die('Access denied.');
+}
 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/link_handler/', 'link handler');
 
-	// hide the button saveDocView for tt_news categories
+// hide the button saveDocView for tt_news categories
 t3lib_extMgm::addUserTSconfig('
 	options.saveDocView.tt_news = 1
 ');
 
+/*
 t3lib_extMgm::addPageTSConfig('
 RTE.default.tx_linkhandler {
 	tt_news {
@@ -25,5 +27,6 @@ mod.tx_linkhandler {
 	}
 }
 ');
+*/
 
 ?>
